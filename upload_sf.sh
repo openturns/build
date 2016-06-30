9a5de2c336bb6bd69cfdadd3872fe78bc859a825
Julien Schueller <schueller@phimeca.com>
2016-06-30 18:37:32 +0200

    Build 1.8rc1

--- upload_sf.sh
+++ upload_sf.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 release=1.8rc1
-for _basename in openturns-${release} otmorris-0.0 otrobopt-0.0 otsvm-0.2
+for _basename in openturns-${release} otfftw-0.3 otlhs-1.3 otmorris-0.1 otpmml-1.3 otrobopt-0.1 otsvm-0.2
 do
   for pybasever in 2.7 3.4 3.5
   do
     for _arch in i686 x86_64
     do
       _file=${_basename}-py${pybasever}-${_arch}.exe
-      wget -c https://github.com/openturns/build/archive/v${release}/${_file} -P /tmp
-      scp /tmp/${_file} $1@frs.sourceforge.net:/home/frs/project/openturns/openturns/openturns-${release}
+      wget -c https://github.com/openturns/build/releases/download/v${release}/${_file} -P /tmp
     done
   done
 done
 
+#       scp /tmp/${_file} $1@frs.sourceforge.net:/home/frs/project/openturns/openturns/openturns-${release}
