daba883396aea5ecd148b432e99f4cf01daad9bf
Julien Schueller <schueller@phimeca.com>
2016-11-18 14:58:44 +0100

    Update upload script

--- upload_sf.sh
+++ upload_sf.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+user=$1
+
 release=1.8
 for _basename in openturns-${release} otfftw-0.3 otlhs-1.3 otmorris-0.1 otpmml-1.3 otrobopt-0.1 otsvm-0.2 otlm-0.2
 do
@@ -7,7 +9,11 @@ do
   version=`echo "${_basename}" | cut -d '-' -f 2`
 
   # source
-  wget -c https://github.com/openturns/${project}/archive/v${version}.tar.gz -P /tmp
+  wget -c https://github.com/openturns/${project}/archive/v${version}.tar.gz -O /tmp/${_basename}.tar.gz
+  if test "${project}" = "openturns"
+  then
+    scp /tmp/${_basename}.tar.gz ${user}@frs.sourceforge.net:/home/frs/project/openturns/openturns/openturns-${release}
+  fi
 
   # exe
   for pybasever in 2.7 3.4 3.5
@@ -16,11 +22,15 @@ do
     do
       _file=${_basename}-py${pybasever}-${_arch}.exe
       wget -c https://github.com/openturns/build/releases/download/v${release}/${_file} -P /tmp
+
+      # upload openturns exe
+      if test "${project}" = "openturns"
+      then
+        scp /tmp/${_file} $1@frs.sourceforge.net:/home/frs/project/openturns/openturns/openturns-${release}
+      fi
     done
   done
 done
 
 sha256sum /tmp/*.exe
 sha256sum /tmp/*.tar.gz
-
-# scp /tmp/${_file} $1@frs.sourceforge.net:/home/frs/project/openturns/openturns/openturns-${release}
